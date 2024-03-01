3cb7c927cf19a1906e79838e8d21a0e772808f2b
Lee Giyeon <rldus3512@naver.com>
2024-03-01 11:37:27 +0900

    project-template 브랜치 생성

---
+++ project-template/src/main/java/ord/teamone/projecttemplate/ProjectTemplateApplication.java
@@ -0,0 +1,13 @@
+package ord.teamone.projecttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTemplateApplication.class, args);
+	}
+
+}
