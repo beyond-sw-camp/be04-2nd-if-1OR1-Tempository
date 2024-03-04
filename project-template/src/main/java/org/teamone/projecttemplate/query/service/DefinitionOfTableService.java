64a661976f74c6c4649841a9080c4d91ce01782f
Lee Giyeon <rldus3512@naver.com>
2024-03-04 13:11:16 +0900

    feat(DefinitionOfTable): 테이블 정의서 주석 정리 및 코드 수정
    1. 프로젝트 ID에 해당하는 테이블 정의서 조회
    2. 테이블 ID에 해당하는 테이블 정의서 조회
    3. 테이블 명에 해당하는 테이블 정의서 조회

--- project-template/src/main/java/org/teamone/projecttemplate/query/service/DefinitionOfTableService.java
+++ project-template/src/main/java/org/teamone/projecttemplate/query/service/DefinitionOfTableService.java
@@ -2,6 +2,7 @@ package org.teamone.projecttemplate.query.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.teamone.projecttemplate.query.dto.DefinitionOfTableDTO;
 import org.teamone.projecttemplate.query.entity.DefinitionOfTable;
 import org.teamone.projecttemplate.query.repository.DefinitionOfTableMapper;
 
@@ -19,7 +20,7 @@ public class DefinitionOfTableService {
     }
 
     /* 설명. Project ID로 해당 테이블 정의서 모두 조회 */
-    public List<DefinitionOfTable> selectAllDefinitionOfTables(org.teamone.projecttemplate.query.dto.DefinitionOfTableDTO definitionDTO) {
+    public List<DefinitionOfTable> selectAllDefinitionOfTables(DefinitionOfTableDTO definitionDTO) {
 
         DefinitionOfTable definitionOfTable = new DefinitionOfTable(definitionDTO.getProjectId());
         List<DefinitionOfTable> result = definitionOfTableMapper.selectAllDefinitionOfTables(definitionOfTable);
@@ -36,7 +37,7 @@ public class DefinitionOfTableService {
     }
 
     /* 설명. Table Name 으로 해당 테이블 정의서 모두 조회 */
-    public List<DefinitionOfTable> selectTableByName(org.teamone.projecttemplate.query.dto.DefinitionOfTableDTO definitionDTO) {
+    public List<DefinitionOfTable> selectTableByName(DefinitionOfTableDTO definitionDTO) {
         DefinitionOfTable definitionOfTable = new DefinitionOfTable(definitionDTO.getTableName());
         List<DefinitionOfTable> result = definitionOfTableMapper.selectDefinitionOfTableByName(definitionOfTable);
 
