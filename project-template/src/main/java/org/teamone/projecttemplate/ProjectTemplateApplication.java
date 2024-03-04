11194ec0d85a80093cda5851145b64218e484c24
B <mox9nox@gmail.com>
2024-03-04 20:19:09 +0900

    fix(ModelMapper): ModelMapper Application으로 위치 수정

--- project-template/src/main/java/org/teamone/projecttemplate/ProjectTemplateApplication.java
+++ project-template/src/main/java/org/teamone/projecttemplate/ProjectTemplateApplication.java
@@ -1,8 +1,10 @@
 package org.teamone.projecttemplate;
 
+import org.modelmapper.ModelMapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan
@@ -10,6 +12,11 @@ public class ProjectTemplateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectTemplateApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
