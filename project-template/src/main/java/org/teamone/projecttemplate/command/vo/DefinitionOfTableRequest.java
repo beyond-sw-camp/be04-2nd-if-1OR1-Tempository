b467bcbc697d5f842b4b3d1af840980886806cf6
Lee Giyeon <rldus3512@naver.com>
2024-03-04 19:01:29 +0900

    feat(DefinitionOfTable): VO Request, Response 추가 생성

---
+++ project-template/src/main/java/org/teamone/projecttemplate/command/vo/DefinitionOfTableRequest.java
@@ -0,0 +1,15 @@
+package org.teamone.projecttemplate.command.vo;
+
+public class DefinitionOfTableRequest {
+    private int id;
+    private int definitionNo;
+    private String tableName;
+    private String propertyName;
+    private boolean primaryKey;
+    private boolean foreignKey;
+    private boolean nullAble;
+    private String columnName;
+    private String defaultValue;
+    private String dataType;
+    private String note;
+}
