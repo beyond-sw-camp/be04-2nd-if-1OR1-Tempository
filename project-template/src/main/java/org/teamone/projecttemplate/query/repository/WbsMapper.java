cf98acf38f5e008d9cd843be8183d2d9065ee092
B <mox9nox@gmail.com>
2024-03-05 11:12:52 +0900

    refactor(Wbs): Mapper 코드 수정

--- project-template/src/main/java/org/teamone/projecttemplate/query/repository/WbsMapper.java
+++ project-template/src/main/java/org/teamone/projecttemplate/query/repository/WbsMapper.java
@@ -1,9 +1,11 @@
 package org.teamone.projecttemplate.query.repository;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.teamone.projecttemplate.query.entity.Wbs;
 
 import java.util.List;
 
+@Mapper
 public interface WbsMapper {
     List<Wbs> selectAllWbsByProjectId(int projectId);
 
