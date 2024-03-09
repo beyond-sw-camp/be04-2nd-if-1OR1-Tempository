9307058c7e92005dc9a1cea7a8cd6d72c70d8d90
DESKTOP-BABBNFL\wj830 <wjdxodndjaak@naver.com>
2024-03-10 04:44:37 +0900

    feat(project): 불필요한 import 제거

--- project/src/main/java/org/teamone/tempository/project/query/entity/ProjectMember.java
+++ project/src/main/java/org/teamone/tempository/project/query/entity/ProjectMember.java
@@ -4,9 +4,7 @@ package org.teamone.tempository.project.query.entity;
 import lombok.*;
 import org.teamone.tempository.project.query.type.ProjectMemberStatus;
 import org.teamone.tempository.project.query.type.ProjectPosition;
-import org.teamone.tempository.project.query.vo.ResponseUser;
 
-import java.util.List;
 
 @Getter
 @NoArgsConstructor
