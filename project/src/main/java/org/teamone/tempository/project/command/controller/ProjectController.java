617cc881318f31dc1b06e953a58bd6b9132ee8cf
LAPTOP-SB0CM2VU\Playdata <wjdxodndjaak@naver.com>
2024-03-14 18:21:12 +0900

    refactor(project): HTTP 메서드 변경

--- project/src/main/java/org/teamone/tempository/project/command/controller/ProjectController.java
+++ project/src/main/java/org/teamone/tempository/project/command/controller/ProjectController.java
@@ -35,7 +35,7 @@ public class ProjectController {
     }
 
     /* 설명. 프로젝트 수정 기능 */
-    @PostMapping("/update")
+    @PatchMapping("/update")
     public String updateProjectById(@RequestBody ProjectDTO newProject) throws IllegalAccessException {
 
         projectService.updateProjectById(newProject);
@@ -45,7 +45,7 @@ public class ProjectController {
     }
 
     /* 설명. 프로젝트 삭제 기능*/
-    @PostMapping("/delete")
+    @DeleteMapping("/delete")
     public String deleteProjectById(@RequestParam("id") String id)  {
 
         projectService.deleteProjectById(id);
