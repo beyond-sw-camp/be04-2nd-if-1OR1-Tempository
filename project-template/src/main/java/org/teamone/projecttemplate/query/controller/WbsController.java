0c6e428b804735522f36aa8333a02e5579883a14
B <mox9nox@gmail.com>
2024-03-20 01:14:15 +0900

    refactor(Wbs): query 쪽 controller 코드 추가 및 수정

--- project-template/src/main/java/org/teamone/projecttemplate/query/controller/WbsController.java
+++ project-template/src/main/java/org/teamone/projecttemplate/query/controller/WbsController.java
@@ -29,6 +29,73 @@ public class WbsController {
         this.modelMapper = modelMapper;
     }
 
+    @GetMapping("/find/{projectId}")
+    public ResponseEntity<List<WbsResponse>> findAllWbsByProjectId(@PathVariable("projectId") int projectId) {
+
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        List<WbsDTO> wbsDTOList = wbsService.findAllWbsByProjectId(projectId);
+
+        List<WbsResponse> wbsResponseList = new ArrayList<>();
+        for (WbsDTO wbsDTO : wbsDTOList) {
+            WbsResponse wbsResponse = modelMapper.map(wbsDTO, WbsResponse.class);
+            wbsResponseList.add(wbsResponse);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(wbsResponseList);
+    }
+
+    @GetMapping("/find/{managerId}")
+    public ResponseEntity<List<WbsResponse>> findWbsByManagerId(@PathVariable("managerId") int managerId) {
+
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        List<WbsDTO> wbsDTOList = wbsService.findWbsByManagerId(managerId);
+
+        List<WbsResponse> wbsResponseList = new ArrayList<>();
+        for (WbsDTO wbsDTO : wbsDTOList) {
+            WbsResponse wbsResponse = modelMapper.map(wbsDTO, WbsResponse.class);
+            wbsResponseList.add(wbsResponse);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(wbsResponseList);
+    }
+
+    @GetMapping("/find/{projectId}/{managerId}")
+    public ResponseEntity<List<WbsResponse>> findWbsByProjectIdAndManagerId(
+            @PathVariable("projectId") int projectId,
+            @PathVariable("managerId") int managerId) {
+
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        WbsDTO wbsDTO = new WbsDTO(projectId, managerId);
+        List<WbsDTO> wbsDTOList = wbsService.findWbsByProjectIdAndManagerId(wbsDTO);
+
+        List<WbsResponse> wbsResponseList = new ArrayList<>();
+
+        for (WbsDTO nextWbsDTO : wbsDTOList) {
+            WbsResponse wbsResponse = modelMapper.map(nextWbsDTO, WbsResponse.class);
+            wbsResponseList.add(wbsResponse);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(wbsResponseList);
+    }
+
+    @GetMapping("/find/{projectId}/{wbsNo}")
+    public ResponseEntity<WbsResponse> findWbsByProjectIdAndWbsNo(
+            @PathVariable("projectId") int projectId,
+            @PathVariable("wbsNo") int wbsNo) {
+
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        WbsDTO wbsDTO = wbsService.findWbsByProjectIdAndWbsNo(projectId, wbsNo);
+
+        WbsResponse wbsResponse = modelMapper.map(wbsDTO, WbsResponse.class);
+
+        return ResponseEntity.status(HttpStatus.OK).body(wbsResponse);
+    }
+
+
 //    @GetMapping("/{projectId}")
 //    public ResponseEntity<List<WbsResponse>> findWbsByProjectId(@PathVariable("projectId") String projectId, @RequestHeader("Authorization") String token) {
 //        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
