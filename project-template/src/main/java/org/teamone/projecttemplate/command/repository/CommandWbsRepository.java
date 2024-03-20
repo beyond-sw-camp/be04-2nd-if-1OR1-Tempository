f2d16ce118ea724da6551cf06cb7487cf93533e0
B <mox9nox@gmail.com>
2024-03-20 16:37:54 +0900

    feat(Wbs): 테스트 코드를 위해 조회 기능 구현
    - 프로젝트 ID, WBS NO를 통한 WBS 조회 기능 구현
    - 프로젝트 ID에 해당하는 WBS 전체 조회 기능 구현

--- project-template/src/main/java/org/teamone/projecttemplate/command/repository/CommandWbsRepository.java
+++ project-template/src/main/java/org/teamone/projecttemplate/command/repository/CommandWbsRepository.java
@@ -2,12 +2,14 @@ package org.teamone.projecttemplate.command.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.teamone.projecttemplate.command.dto.CommandWbsDTO;
 import org.teamone.projecttemplate.command.entity.CommandWbs;
 
 import java.util.List;
 
 @Repository
 public interface CommandWbsRepository extends JpaRepository<CommandWbs, Integer> {
+
     List<CommandWbs> findByProjectId(int projectId);
 
     CommandWbs findByProjectIdAndWbsNo(int projectId, int wbsNo);
