626797f4120d0fe4a5d7397a65750fe71ae7e052
moomint8 <office05053@gmail.com>
2024-03-21 18:32:25 +0900

    refactor(user): 일부 클래스 디렉토리 위치 변경
    CQRS 패턴에 더 적합하도록 일부 클래스의 디렉토리 위치가 변경되었습니다.

--- user/src/main/java/org/teamone/user/command/Application/service/CommandUserAuthServiceImpl.java
+++ user/src/main/java/org/teamone/user/command/Application/service/CommandUserAuthServiceImpl.java
@@ -1,4 +1,4 @@
-package org.teamone.user.command.domain.service;
+package org.teamone.user.command.Application.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
